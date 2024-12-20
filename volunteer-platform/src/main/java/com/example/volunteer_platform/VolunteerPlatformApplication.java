102d3596c5c72c9ed9f0fe8ce19e8773a9e6d2b2
tbui73 <tbui73@gatech.edu>
2024-12-20 10:27:30 -0500

    Initialized Spring Boot project with Spring Initializer

---
+++ volunteer-platform/src/main/java/com/example/volunteer_platform/VolunteerPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.volunteer_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteerPlatformApplication.class, args);
+	}
+
+}
